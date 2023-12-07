e2b4f3a078dde5b8f929f1fe94442211d499e91a
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-12-07 21:48:53 +0900

    Intro 스타일 컴포넌트 구조로 작업 진행

---
+++ src/components/templates/template.style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Layout = styled.div`
+  height: 100%;
+  font-weight: bold;
+  padding: 0 1.5rem;
+  max-width: 380px;
+  margin: 0 auto;
+`;
--- src/pages/Intro/Intro.js
+++ src/pages/Intro/Intro.js
@@ -1,131 +1,24 @@
 import React from "react";
-import styled from "styled-components";
 import cardChristmasTreeImage from "../../assets/trees/card-cristmas-tree.png";
 import letterImage from "../../assets/letter/letter.png";
 import socksImage from "../../assets/socks/socks.png";
 import { FiShare } from "react-icons/fi";
+import * as TS from "../../components/templates/template.style";
+import * as IntroS from "./Intro.style";
 
-const Layout = styled.div`
-  height: 100%;
-  font-weight: bold;
-  padding: 0 1.5rem;
-  max-width: 380px;
-
-  margin: 0 auto;
-`;
-
-const Header = styled.header`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  padding: 4.2rem 0;
-  & h1,
-  & p {
-    font-size: 1.5rem;
-  }
-  & h1 {
-    margin-bottom: 1rem;
-    font-family: serif;
-  }
-  & h2 {
-    font-family: "Hurricane", cursive;
-    font-weight: 400;
-    font-size: 3.1rem;
-    margin-bottom: 1.7rem;
-  }
-`;
-
-const Main = styled.main``;
-
-const Card = styled.section`
-  position: relative;
-  width: 100%;
-  height: 330px;
-  &.red {
-    background: linear-gradient(193deg, rgba(242, 76, 54, 1) 0%, rgba(187, 37, 40, 1) 80%);
-  }
-  &.blue {
-    background: linear-gradient(193deg, rgba(54, 197, 242, 1) 0%, rgba(54, 152, 242, 1) 80%);
-  }
-  &.green {
-    background: linear-gradient(193deg, rgba(123, 175, 98, 1) 0%, rgba(55, 170, 111, 1) 80%);
-  }
-  border-radius: 25px;
-  color: #ffffff;
-  padding: 1.5rem 0.8rem;
-  margin-bottom: 1rem;
-  & h2 {
-    font-size: 1.65rem;
-    margin-bottom: 1rem;
-  }
-  & p {
-    font-weight: lighter;
-    font-size: 18px;
-  }
-  &.red {
-    img {
-      position: absolute;
-      width: 100%;
-      height: 500px;
-      object-fit: contain;
-      top: -70px;
-    }
-  }
-
-  &.blue {
-    img {
-      position: absolute;
-      width: 100%;
-      height: 500px;
-      object-fit: contain;
-      top: -20px;
-    }
-  }
-  &.green {
-    img {
-      position: absolute;
-      width: 100%;
-      height: 250px;
-      object-fit: contain;
-      right: -60px;
-      bottom: -30px;
-    }
-  }
-`;
-
-const Footer = styled.footer`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 1.8rem 0;
-`;
-
-const Button = styled.button`
-  width: 199px;
-  padding: 1.3rem 1rem;
-  border-radius: 0.625rem;
-  margin-bottom: 1rem;
-  color: #ffffff;
-  font-size: 1.4rem;
-  border: none;
-  ${(props) => props.bgColor && `background-color: ${props.bgColor};`}
-  cursor: pointer;
-`;
 function Intro() {
   return (
-    <Layout>
-      <Header>
+    <TS.Layout>
+      <IntroS.Header>
         <h1>D-1</h1>
         <h2>Christmas in Card</h2>
         <p>
           이번 크리스마스에 <br />
           함께 할 친구를 소개해드려요
         </p>
-      </Header>
-      <Main>
-        <Card className="red">
+      </IntroS.Header>
+      <IntroS.Main>
+        <IntroS.Card className="red">
           <h2>캐릭터를 만들어요</h2>
           <p>
             나만의 크리스마스
@@ -134,8 +27,8 @@ function Intro() {
             <br /> 이야기를 나눠봐요
           </p>
           <img src={cardChristmasTreeImage} alt="애플리케이션 소개 카드 중 크리스마스 트리 이미지" />
-        </Card>
-        <Card className="blue">
+        </IntroS.Card>
+        <IntroS.Card className="blue">
           <h2>마음을 나눠보세요</h2>
           <p>
             2023년을 마무리하며 친구들에게
@@ -145,8 +38,8 @@ function Intro() {
             드려요
           </p>
           <img src={letterImage} alt="애플리케이션 소개 카드 중 크리스마스 트리 이미지" />
-        </Card>
-        <Card className="green">
+        </IntroS.Card>
+        <IntroS.Card className="green">
           <h2>선물도 함께 받아보세요</h2>
           <p>
             이번 크리스마스는
@@ -158,16 +51,16 @@ function Intro() {
             선물해 드려요
           </p>
           <img src={socksImage} alt="애플리케이션 소개 카드 중 크리스마스 트리 이미지" />
-        </Card>
-      </Main>
-      <Footer>
-        <Button bgColor="#FF5D56">캐릭터 받기</Button>
-        <Button bgColor="#BFBFBF">
+        </IntroS.Card>
+      </IntroS.Main>
+      <IntroS.Footer>
+        <IntroS.Button bgColor="#FF5D56">캐릭터 받기</IntroS.Button>
+        <IntroS.Button bgColor="#BFBFBF">
           <FiShare />
           이벤트 공유
-        </Button>
-      </Footer>
-    </Layout>
+        </IntroS.Button>
+      </IntroS.Footer>
+    </TS.Layout>
   );
 }
 
